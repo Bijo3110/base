447f28edbd27a23ef2543bb57b14b1077cfb16de
Bijo <3110.bijo@gmail.com>
2026-01-31 13:13:02 +0530

    Changes in Models file

--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 #Create your models here.
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published', auto_now=True)
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
 
     def __str__(self):
-        return self.question_text
+        return self.title
